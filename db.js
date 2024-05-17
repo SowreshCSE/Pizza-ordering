c9b090ae87f69cb3492193823c8819fe21fc8544
SowreshCSE <sowresh.k2021csec@sece.ac.in>
2024-05-17 19:59:52 +0530

    url updated

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb+srv://sowresh:sowresh18@pizza.c0mpkgh.mongodb.net/?retryWrites=true&w=majority&appName=pizza');
+
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
